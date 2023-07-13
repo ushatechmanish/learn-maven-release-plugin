e550766a76b1dd01cbc56bbc1b499845776f0221
Manish Chauhan <manish.chauhan@ushatech.in>
2023-07-13 19:41:58 +0530

    Initial Commit trying to use maven plugin.

---
+++ src/main/java/in/ushatech/learnmavenreleaseplugin/LearnMavenReleasePluginApplication.java
@@ -0,0 +1,13 @@
+package in.ushatech.learnmavenreleaseplugin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnMavenReleasePluginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnMavenReleasePluginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/in/ushatech/learnmavenreleaseplugin/LearnMavenReleasePluginApplicationTests.java
@@ -0,0 +1,13 @@
+package in.ushatech.learnmavenreleaseplugin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnMavenReleasePluginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
